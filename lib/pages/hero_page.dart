180886788523fe6597b6099463432436f75ecf15
bizz84 <bizz84@gmail.com>
2019-09-06 12:25:02 +0100

    Add FutureBuilder code

--- lib/pages/hero_page.dart
+++ lib/pages/hero_page.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 class HeroHeader implements SliverPersistentHeaderDelegate {
   HeroHeader({
@@ -80,16 +81,100 @@ class HeroPage extends StatelessWidget {
               maxExtent: 250.0,
             ),
           ),
-          SliverFillRemaining(
-            child: Center(
-              child: Text(
-                'No Content',
-                style: Theme.of(context).textTheme.headline,
-              ),
-            ),
-          ),
+          HeroBody(),
+          // SliverFillRemaining(
+          //   child: Center(
+          //     child: Text(
+          //       'No Content',
+          //       style: Theme.of(context).textTheme.headline,
+          //     ),
+          //   ),
+          // ),
         ],
       ),
+    );
+  }
+}
+
+class HeroBody extends StatefulWidget {
+  @override
+  _HeroBodyState createState() => _HeroBodyState();
+}
+
+class _HeroBodyState extends State<HeroBody> {
+  Future<String> _loader;
+  bool _shouldFail = false;
+
+  Future<String> getData(bool shouldFail) async {
+    await Future<void>.delayed(Duration(seconds: 3));
+    if (_shouldFail) {
+      throw PlatformException(code: '404');
+    }
+    return 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
+  }
+
+  void retry() {
+    _loader = getData(!_shouldFail);
+    setState(() {
+      _shouldFail = !_shouldFail;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loader = getData(_shouldFail);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<String>(
+      future: _loader,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return SliverFillRemaining(
+            child: Center(child: CircularProgressIndicator()),
+          );
+        }
+        if (snapshot.hasError) {
+          return SliverFillRemaining(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('An error occurred'),
+              RaisedButton(
+                child: Text('Retry'),
+                onPressed: () => retry(),
+              ),
+            ],
+          ));
+        }
+        if (snapshot.hasData) {
+          // show something
+          return SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    snapshot.data,
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                  RaisedButton(
+                    child: Text('Reload'),
+                    onPressed: () => retry(),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+        // show placeholder
+        return SliverFillRemaining(
+          child: Center(child: Text('No Content')),
+        );
+      },
     );
   }
 }
--- lib/pages/slivers_basic_page.dart
+++ lib/pages/slivers_basic_page.dart
@@ -14,11 +14,49 @@ class SliversBasicPage extends StatelessWidget {
             title: Text('Slivers'),
           ),
         ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return Container(
+                height: 50,
+                alignment: Alignment.center,
+                color: Colors.orange[100 * (index % 9)],
+                child: Text('orange $index'),
+              );
+            },
+            childCount: 9,
+          ),
+        ),
+        // SliverFixedExtentList(
+        //   itemExtent: 50.0,
+        //   delegate: SliverChildBuilderDelegate(
+        //     (context, index) {
+        //       return Container(
+        //         alignment: Alignment.center,
+        //         color: Colors.lightBlue[100 * (index % 9)],
+        //         child: Text('list item $index'),
+        //       );
+        //     },
+        //   ),
+        // ),
+        SliverToBoxAdapter(
+          child: Container(
+            color: Colors.yellow,
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Grid Header'),
+          ),
+        ),
         SliverGrid(
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200.0,
-            mainAxisSpacing: 10.0,
-            crossAxisSpacing: 10.0,
+          // gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          //   maxCrossAxisExtent: 200.0,
+          //   mainAxisSpacing: 10.0,
+          //   crossAxisSpacing: 10.0,
+          //   childAspectRatio: 4.0,
+          // ),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
             childAspectRatio: 4.0,
           ),
           delegate: SliverChildBuilderDelegate(
@@ -30,18 +68,6 @@ class SliversBasicPage extends StatelessWidget {
               );
             },
             childCount: 20,
-          ),
-        ),
-        SliverFixedExtentList(
-          itemExtent: 50.0,
-          delegate: SliverChildBuilderDelegate(
-            (context, index) {
-              return Container(
-                alignment: Alignment.center,
-                color: Colors.lightBlue[100 * (index % 9)],
-                child: Text('list item $index'),
-              );
-            },
           ),
         ),
       ],
